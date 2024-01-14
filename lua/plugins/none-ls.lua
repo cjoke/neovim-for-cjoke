b10e6233eee5f366d1cec7b94246dc2c2aafde01
cj <cjokee@gmail.com>
2024-01-14 20:01:36 +0100

    added some formatters in none-ls
    json, xml, yaml
    auto reorder imports in python pyflyby

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,9 +7,15 @@ return {
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.diagnostics.flake8,
+        null_ls.builtins.code_actions.gitsigns,
+        null_ls.builtins.formatting.json_tool,
+        null_ls.builtins.formatting.pyflyby,
+        null_ls.builtins.formatting.reorder_python_imports,
+        null_ls.builtins.formatting.xmlformat,
+        null_ls.builtins.formatting.yamlfix,
 			},
 		})
 		local keymap = vim.keymap.set
-		keymap("n", "<leader>gf", vim.lsp.buf.format, { desc = "[G]o [F]ormat" })
+		keymap("n", "<leader>gf", vim.lsp.buf.format, { desc = "Go Format" })
 	end,
 }
