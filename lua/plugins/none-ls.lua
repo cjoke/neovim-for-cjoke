af855aa2b5871c6953f0c48d176e556d9428961e
cj <cjokee@gmail.com>
2024-01-15 14:16:26 +0100

    lazygit and none-ls conf

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,12 +7,10 @@ return {
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.diagnostics.flake8,
+        null_ls.builtins.diagnostics.cppcheck,
         null_ls.builtins.code_actions.gitsigns,
         null_ls.builtins.formatting.json_tool,
-        null_ls.builtins.formatting.pyflyby,
-        null_ls.builtins.formatting.reorder_python_imports,
-        null_ls.builtins.formatting.xmlformat,
-        null_ls.builtins.formatting.yamlfix,
+        null_ls.builtins.formatting.prettier,
 			},
 		})
 		local keymap = vim.keymap.set
