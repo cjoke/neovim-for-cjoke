36c4a6f2d428a1684d6907a95d184e94d9f04d7e
cj <cjokee@gmail.com>
2024-02-20 10:15:30 +0100

    none-ls.lua
    minor changes

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,14 +7,12 @@ return {
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.diagnostics.flake8,
-        -- null_ls.builtins.diagnostics.cppcheck,
-        null_ls.builtins.diagnostics.clangd,
+        null_ls.builtins.diagnostics.cppcheck,
+        -- null_ls.builtins.diagnostics.clangd,
         null_ls.builtins.code_actions.gitsigns,
         null_ls.builtins.formatting.json_tool,
         null_ls.builtins.formatting.prettier,
       },
     })
-    local keymap = vim.keymap.set
-    keymap("n", "<leader>gf", vim.lsp.buf.format, { desc = "Go Format" })
   end,
 }
